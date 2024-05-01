235328868220234d052bbc827e748c052033cdac
Asaf Avenstein <asafavenstein@Asafs-Air.lan>
2024-05-01 15:25:56 +0300

    wip

--- web-flask/web.py
+++ web-flask/web.py
@@ -1,8 +1,13 @@
 from flask import Flask
 
 app = Flask(__name__)
-
-
+ 
 @app.route('/')
-def hello():
-    return 'Hello, World!'
+
+def hello_world():
+    return 'Hello World'
+ 
+
+if __name__ == '__main__':
+ 
+    app.run()
