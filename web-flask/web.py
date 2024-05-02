e65804ab58b468e9b52d02b474adc336e90924f8
Asaf Avenstein <asafavenstein@Asafs-Air.lan>
2024-05-02 15:11:40 +0300

    wip

--- web-flask/web.py
+++ web-flask/web.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import socket
 app = Flask(__name__)
 
 # two decorators, same function
@@ -7,5 +8,6 @@ def index():
     return render_template('index.html', the_title='Index Home Page')
 
 if __name__ == '__main__':
+    ip_addr = socket.gethostbyname(socket.gethostname())
     port = 6000
     app.run(debug=True, host='0.0.0.0', port=port)
